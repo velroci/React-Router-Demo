7a98788ea96780a4c5c561efe6c29d6d893b3c4e
velroci <rossielpz@gmail.com>
2024-02-01 19:40:30 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+    </div>
+    </>
+  )
+}
+export default App
